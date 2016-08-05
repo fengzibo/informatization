8c2d02039d915151820c199e1b0a08457654be83
吴临波 <wulinbo@zibolan.com>
2016-08-05 08:44:39 +0800

    教学课程内容功能填充

--- src/js/Common.js
+++ src/js/Common.js
@@ -60,6 +60,13 @@ var Common = {
             $('.tab-content').eq(thisi).stop().fadeIn().siblings('.tab-content').fadeOut();
         })
     },
+    tabcut2: function () {
+        $('.dis-mes-tab').find('li').on('click', function () {
+            var thisi = $(this).index();
+            $(this).addClass('current').siblings().removeClass('current');
+            $('.dispaly-message').eq(thisi).show().siblings('.dispaly-message').hide();
+        })
+    },
     //密码强度显示
     pasgrade: function () {
         $('.pasgrade').keyup(function () {
@@ -86,6 +93,7 @@ var Common = {
             $(this).find('a').addClass('current').parent().siblings().find('a').removeClass('current');
             $('.ification-content').html(Common.commonblock(x[thisi]));
             Common.footerbottom();
+            $(".select").select2();
         })
     },
     //收藏状态
@@ -141,5 +149,153 @@ var Common = {
             $(".select").select2();
         })
     },
+    //倒计时
+    showTime: function (time,fn) {
+        function show_time(){
+            if(time>=0){
+                d = Math.floor(time/3600/24);
+                h = Math.floor((time/3600)%24);
+                m = Math.floor((time/60)%60);
+                s = Math.floor(time%60);
+                if(d < 10){
+                    d = "0" + d;
+                }
+                if(h < 10){
+                    h = "0" + h;
+                }
+                if(m < 10){
+                    m = "0" + m;
+                }
+                if(s < 10){
+                    s = "0" + s;
+                }
+                var msg ;
+                fn(msg);
+                if(time == 5*60){
+                    alert('请注意,距离考试结束还有5分钟');
+                }
+                time--;
+            }else {
+                clearInterval(timer);
+                alert('时间到,结束')
+            }
+        }
+        show_time();
+        var timer = setInterval(function () {
+            show_time();
+        },1000);
+    },
+    //考试题目翻页
+    page_topic:function (){
+        var next = $('#next-topic');
+        var prev = $('#prev-topic');
+        var _Link = $('.topic-item li span');
+        var now =  parseInt(_Link.index($('.topic-item li span.current')));
+        _Link.eq(0).addClass('current');
+        function process(_now,_Prev,_Next){
+            _Link.removeClass('current');
+            _Link.eq(_now).addClass('current');
+            var total = parseInt($('.topic-item li span').length - 1);
+            if(_now == 0){
+                _Prev.hide();
+                _Next.show();
+            }
+            else if(_now == total){
+                _Prev.show();
+                _Next.hide();
+            }
+            else {
+                _Prev.show();
+                _Next.show();
+            }
+        }
+        process(0,prev,next);
+        next.click(function(){
+            process(now+1,prev,next);
+            now =  parseInt(_Link.index($('.topic-item li span.current')));
+        });
+        prev.click(function(){
+            process(now-1,prev,next);
+            now =  parseInt(_Link.index($('.topic-item li span.current')));
+        })
+        _Link.click(function(){
+            var that = $(this);
+            _Link.removeClass('current');
+            that.addClass('current');
+            now =  parseInt(_Link.index($('.topic-item li span.current')));
+            process(now,prev,next);
+            console.log(_Link.index($('.topic-item li span.current')));
+        })
+
+    },
+    //组员所有功能
+    teamfuc: function () {
+        $('.crew-menu').on('click', function (e) {
+            if($('.crew-content').is(':hidden')){
+                $(this).find('span').css({backgroundColor:"#ff503f",color:"#ffffff"})
+                $(this).find('span').stop().animate({height:'700px'},300);
+                $('.crew-content').stop().show(300);
+                $('.task-back').on('click',function(e){
+                    $('.task-main').hide();
+                    $('.task-item').show();
+                    e.stopPropagation();
+                });
+                $('.task-dea ul li').on('click',function(e){
+                    $('.task-item').hide();
+                    $('.task-main').show();
+                    Common.tabcut2();
+                    var texra = $('.input-content textarea.dis-mes-val');
+                    var ulwidth = 0;
+                    function send_mes(){
+                        if(texra.val() != ''){
+                            var d = new Date();
+                            var h = d.getHours();
+                            var m = d.getMinutes();
+                            if(h<10){
+                                h = '0'+h;
+                            };
+                            if(m<10){
+                                m = '0'+m;
+                            };
+                            var html = "<div class='textformes'><div class='mem-name'>sadada:</div><p>"+texra.val()+"<span>"+h+":"+m+"</span></p></div>";
+                            $('.dis-mes-box').append(html);
+                            texra.val('');
+                        }else {
+                            alert('不能发送空内容');
+                            texra.val('');
+                        };
+                    };
+                    $('.x-scroll ul.annex li').each(function () {
+                        ulwidth += parseInt($(this).outerWidth(true));
+                    });
+                    $(".x-scroll ul.annex").width(ulwidth);
+                    $('.sendbox a').on('click', function () {
+                        send_mes();
+                    })
+                    texra.keydown(function (event) {
+                        if(event.keyCode == 13){
+                            send_mes();
+                        }
+                    });
+                    $('.task-main-right ul.team-members li').on('click', function (e) {
+                        $('#group-task').show();
+                        $('#group-task').find('h2').html("组员["+$(this).text()+"]任务"+"<a class='iconfont icon-guanbi closelayer'>退出</a>")
+                        $('.closelayer').click(function (e) {
+                            $(this).parents('#group-task').hide();
+                            $('.task-size .write-task').val('');
+                            e.stopPropagation();
+                        })
+                        e.stopPropagation();
+                    })
+                    e.stopPropagation();
+                });
+            }else {
+                $(this).find('span').css({backgroundColor:"#e8e8e8",color:"#3c3c3c"})
+                $(this).find('span').stop().animate({height:'150px'},300);
+                $('.crew-content').stop().hide(300);
+            };
+            e.stopPropagation();
+        });
+    }
 
 }
